370db1278720c5fcd505537477bd6ccf8e1a714e
Boyina, Manmohan <boyinamanmohan@gmail.com>
2025-03-17 14:06:42 -0400

    Added toggle code

--- script.js
+++ script.js
@@ -96,3 +96,13 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 });
+
+// const toggleSwitch = document.getElementById('switch');
+
+// toggleSwitch.addEventListener('change', function() {
+//   if (toggleSwitch.checked) {
+//     document.body.classList.add('dark-mode');
+//   } else {
+//     document.body.classList.remove('dark-mode');
+//   }
+// });
